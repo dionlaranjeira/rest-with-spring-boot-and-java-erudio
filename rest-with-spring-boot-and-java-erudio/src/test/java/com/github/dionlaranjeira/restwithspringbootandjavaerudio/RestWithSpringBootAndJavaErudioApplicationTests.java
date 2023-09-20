7164a89cffa0fdc9e06975139b47e2083c1768de
dion.laranjeira <dion.laranjeira@riocardmais.com.br>
2023-09-19 23:44:02 -0300

    [FEAT] Startando o projeto com endpoint /greeting

---
+++ rest-with-spring-boot-and-java-erudio/src/test/java/com/github/dionlaranjeira/restwithspringbootandjavaerudio/RestWithSpringBootAndJavaErudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dionlaranjeira.restwithspringbootandjavaerudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootAndJavaErudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
