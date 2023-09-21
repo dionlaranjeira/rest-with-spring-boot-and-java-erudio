bf2f0491fae5795b9e7d5e84dfdf3e7891219965
dion.laranjeira <dion.laranjeira@riocardmais.com.br>
2023-09-20 22:53:29 -0300

    [FEAT] Endpoint /sum/{numberOne}/{numberTwo} (soma dois números) Sem tratamento de exceção.

---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/com/github/dionlaranjeira/restwithspringbootandjavaerudio/MathController.java
@@ -0,0 +1,40 @@
+package com.github.dionlaranjeira.restwithspringbootandjavaerudio;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class MathController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double sum(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new Exception();}
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private Double convertToDouble(String strNumber) {
+        if(strNumber == null) return 0D;
+
+        String number = strNumber.replaceAll(",",".");
+        if(isNumeric(number)) return Double.parseDouble(number);
+
+        return 0D;
+    }
+
+    private boolean isNumeric(String strNumber) {
+        if (strNumber == null) return false;
+        String number = strNumber.replaceAll(",",".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+
+}
